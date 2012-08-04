85350b17756852a3cae6122d5c93f14ff08fc474
ShogunPanda <shogun_panda@me.com>
2012-08-04 08:43:44 +0200

    Updated pry dependency

--- lib/elephas/version.rb
+++ lib/elephas/version.rb
@@ -17,7 +17,7 @@ module Elephas
     MINOR = 1
 
     # The patch version.
-    PATCH = 0
+    PATCH = 1
 
     # The current version of elephas.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
