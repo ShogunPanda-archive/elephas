0ca57342c82aff0fa1ba8ae95ece5f7181a44747
ShogunPanda <shogun_panda@me.com>
2013-07-14 15:24:56 -0700

    Version 4.1.4

--- lib/elephas/version.rb
+++ lib/elephas/version.rb
@@ -17,7 +17,7 @@ module Elephas
     MINOR = 1
 
     # The patch version.
-    PATCH = 3
+    PATCH = 4
 
     # The current version of elephas.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
